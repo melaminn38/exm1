3aaa02b14631caa6807fc3fd9001e6bb156dad88
mel <melaminn38@gmail.com>
2024-08-19 22:29:07 +0900

    exmple commit springboot 프로젝트

---
+++ main/java/com/example/demo/SpringConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import com.example.demo.repository.JdbcMemberRepository;
+import com.example.demo.repository.JdbcTemplateMemberRepository;
+import com.example.demo.repository.JpaMemberRepository;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//       // return new JdbcMemberRepository(dataSource);
+//       // return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+}
